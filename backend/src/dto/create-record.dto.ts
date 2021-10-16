f24b0bd5f0663848d163dec8698d372d1ec555b1
Sebastian Springer <88656+sspringer82@users.noreply.github.com>
2021-10-16 10:58:14 +0200

    schema doc for create dto

--- backend/src/dto/create-record.dto.ts
+++ backend/src/dto/create-record.dto.ts
@@ -1,6 +1,27 @@
+import { ApiProperty } from '@nestjs/swagger';
+
 export class CreateRecordDto {
+  @ApiProperty({
+    example: 1613286000000,
+    description: 'The start date and time of the record',
+  })
   readonly start: number;
+
+  @ApiProperty({
+    example: 1613293200000,
+    description: 'The end date and time of the record',
+  })
   readonly end: number;
+
+  @ApiProperty({
+    example: 'Breakfast',
+    description: 'The name of the project',
+  })
   readonly project: string;
+
+  @ApiProperty({
+    example: 'Coffee and toast',
+    description: 'Additional Information',
+  })
   readonly comment?: string;
 }
