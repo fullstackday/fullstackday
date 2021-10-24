539d08587fb86ad38611f8e0db53d0d5937ae1b5
Hans-Christian Otto <c.otto@suora.com>
2021-10-24 13:15:16 +0200

    Initialised React App

---
+++ client-react/src/App.tsx
@@ -0,0 +1,13 @@
+import { useState } from 'react'
+
+function App() {
+  const [count, setCount] = useState(0)
+
+  return (
+    <div className="App">
+      Hello iJS!
+    </div>
+  )
+}
+
+export default App
