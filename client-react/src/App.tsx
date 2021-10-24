8e96bb38498146468c1f5f144e43a42a50c85e24
Hans-Christian Otto <c.otto@suora.com>
2021-10-24 13:20:44 +0200

    Added prettier

--- client-react/src/App.tsx
+++ client-react/src/App.tsx
@@ -1,18 +1,16 @@
-import CssBaseline from '@mui/material/CssBaseline';
-import '@fontsource/roboto/300.css';
-import '@fontsource/roboto/400.css';
-import '@fontsource/roboto/500.css';
-import '@fontsource/roboto/700.css';
-
+import CssBaseline from "@mui/material/CssBaseline";
+import "@fontsource/roboto/300.css";
+import "@fontsource/roboto/400.css";
+import "@fontsource/roboto/500.css";
+import "@fontsource/roboto/700.css";
 
 function App() {
-
   return (
     <>
       <CssBaseline />
       Hello iJS!
     </>
-  )
+  );
 }
 
-export default App
+export default App;
