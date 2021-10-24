0335798a373f8b0b70c8915e6a40f3fb8e9a5fc0
Hans-Christian Otto <c.otto@suora.com>
2021-10-24 13:19:23 +0200

    Prepare MUI

--- client-react/src/App.tsx
+++ client-react/src/App.tsx
@@ -1,12 +1,17 @@
-import { useState } from 'react'
+import CssBaseline from '@mui/material/CssBaseline';
+import '@fontsource/roboto/300.css';
+import '@fontsource/roboto/400.css';
+import '@fontsource/roboto/500.css';
+import '@fontsource/roboto/700.css';
+
 
 function App() {
-  const [count, setCount] = useState(0)
 
   return (
-    <div className="App">
+    <>
+      <CssBaseline />
       Hello iJS!
-    </div>
+    </>
   )
 }
 
