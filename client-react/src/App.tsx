1e15b3ea4a05daa05819e63877355c9d5b4ec2cc
Hans-Christian Otto <c.otto@suora.com>
2021-10-25 11:10:18 +0200

    Restructured App a bit

--- client-react/src/App.tsx
+++ client-react/src/App.tsx
@@ -1,4 +1,3 @@
-import CssBaseline from "@mui/material/CssBaseline";
 import "@fontsource/roboto/300.css";
 import "@fontsource/roboto/400.css";
 import "@fontsource/roboto/500.css";
@@ -6,20 +5,15 @@ import "@fontsource/roboto/700.css";
 import { Layout } from "./component/Layout";
 import { TimeRecordsScene } from "./component/TimeRecordsScene";
 import { QueryClient, QueryClientProvider } from "react-query";
-import AdapterDateFns from "@mui/lab/AdapterDateFns";
-import LocalizationProvider from "@mui/lab/LocalizationProvider";
 
 const queryClient = new QueryClient();
 
 function App() {
   return (
     <QueryClientProvider client={queryClient}>
-      <LocalizationProvider dateAdapter={AdapterDateFns}>
-        <Layout>
-          <CssBaseline />
-          <TimeRecordsScene />
-        </Layout>
-      </LocalizationProvider>
+      <Layout>
+        <TimeRecordsScene />
+      </Layout>
     </QueryClientProvider>
   );
 }
--- client-react/src/component/Layout.tsx
+++ client-react/src/component/Layout.tsx
@@ -3,20 +3,26 @@ import AppBar from "@mui/material/AppBar";
 import Box from "@mui/material/Box";
 import Toolbar from "@mui/material/Toolbar";
 import Typography from "@mui/material/Typography";
+import AdapterDateFns from "@mui/lab/AdapterDateFns";
+import { LocalizationProvider } from "@mui/lab";
+import CssBaseline from "@mui/material/CssBaseline";
 
 interface Props {}
 
 export const Layout: React.FunctionComponent<Props> = ({ children }) => {
   return (
-    <Box sx={{ flexGrow: 1 }}>
-      <AppBar position="static">
-        <Toolbar>
-          <Typography variant="h6" component="div" sx={{ flexGrow: 1 }}>
-            WorkSchedule
-          </Typography>
-        </Toolbar>
-      </AppBar>
-      {children}
-    </Box>
+    <LocalizationProvider dateAdapter={AdapterDateFns}>
+      <Box sx={{ flexGrow: 1 }}>
+        <CssBaseline />
+        <AppBar position="static">
+          <Toolbar>
+            <Typography variant="h6" component="div" sx={{ flexGrow: 1 }}>
+              WorkSchedule
+            </Typography>
+          </Toolbar>
+        </AppBar>
+        {children}
+      </Box>
+    </LocalizationProvider>
   );
 };
